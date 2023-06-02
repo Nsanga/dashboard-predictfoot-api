b84599e2057586d994df3296e58a0554386c4cf9
Mercure Mekinda <mercuremekinda@gmail.com>
2023-06-02 15:16:00 +0100

    update crud landing page

--- api/controllers/landingPage/plan.controller.js
+++ api/controllers/landingPage/plan.controller.js
@@ -1,31 +1,31 @@
-var ServiceFlatrate = require('../../services/landingPage/plan.service');
+var ServicePlan = require('../../services/landingPage/plan.service');
 
 
 const getAll = async (req, res) => {
      // Get all plans
-     await ServiceFlatrate.getAll(req, res);
+     await ServicePlan.getAll(req, res);
 
 };
 
 const getOne = async (req, res) => {
   // Get a plan
-  await ServiceFlatrate.getOne(req, res);
+  await ServicePlan.getOne(req, res);
 
 };
 
 const create = async (req, res) => {
      // Create a plan
-     await ServiceFlatrate.create(req, res);
+     await ServicePlan.create(req, res);
 };
 
 const update = async (req, res) => {
      // Modify a plan
-     await ServiceFlatrate.update(req, res);
+     await ServicePlan.update(req, res);
 };
 
 const deleted = async (req, res) => {
      // Delete a plan
-     await ServiceFlatrate.deleted(req, res);
+     await ServicePlan.deleted(req, res);
 };
 
 module.exports = { getAll, getOne, create, update, deleted };
