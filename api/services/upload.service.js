54950f1a2f80138ccd08dd4f2bd7ecd0516b9ab3
Mercure Mekinda <mercuremekinda@gmail.com>
2023-06-06 15:32:59 +0100

    crud landing page at file home

--- api/services/upload.service.js
+++ api/services/upload.service.js
@@ -1,45 +1,56 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
+
 const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
 const accessKey = process.env.AWS_ACCESS_KEY;
 const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 AWS.config.update({ accessKeyId: accessKey, secretAccessKey: secretKey });
 const s3 = new AWS.S3();
-console.log('ok::', s3);
+console.log('ok::', s3)
 
-const uploadFile = (files, folder) => {
-  if (!Array.isArray(files)) {
-    files = [files]; // Convertit en tableau si ce n'est pas déjà le cas
-  }
-
-  const uploadPromises = files.map((file) => {
+const uploadFile = (file, folder) => {
     return new Promise((resolve, reject) => {
-      const fileStream = fs.createReadStream(file.path);
-      fileStream.on('error', function (err) {
-        reject(err);
-      });
+        const fileStream = fs.createReadStream(file.path);
+        fileStream.on('error', function (err) {
+            reject(err);
+        });
 
-      const params = {
-        Bucket: bucketName,
-        Key: `${folder}/${file.originalname}`,
-        Body: fileStream,
-      };
+        const params = {
+            Bucket: bucketName,
+            Key: `${folder}/${file.originalname}`,
+            Body: fileStream,
+        };
 
-      s3.upload(params, function (err, data) {
-        console.log('ok::', data);
-        if (err) {
-          reject(err);
-        } else {
-          const imageUrl = data.Location;
-          console.log('Uploaded image URL:', imageUrl);
-          resolve(imageUrl);
-        }
-      });
+        s3.upload(params, function (err, data) {
+            console.log('ok::', data)
+            if (err) {
+                reject(err);
+            } else {
+                const imageUrl = data.Location;
+                console.log('Uploaded image URL:', imageUrl);
+                resolve(imageUrl);
+            }
+        });
     });
-  });
-
-  return Promise.all(uploadPromises);
 };
 
-exports.uploadFile = uploadFile;
+exports.uploadFile = uploadFile
+
+const uploadFiles = async (files, folder) => {
+    const uploadedFiles = [];
+
+    for (const file of files) {
+        try {
+            const imageUrl = await uploadFile(file, folder);
+            uploadedFiles.push(imageUrl);
+        } catch (error) {
+            console.error(`Failed to upload file: ${file.originalname}`, error);
+        }
+    }
+
+    return uploadedFiles;
+};
+
+exports.uploadFiles = uploadFiles
